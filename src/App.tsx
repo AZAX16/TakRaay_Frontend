6ba483a15ca6a404255b22906ee4ecf40d7fbf05
MAHI <mvahedi@live.com>
2026-05-29 19:34:28 +0330

    final modal

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+// import TestProfile from "./pages/TestProfile.tsx";
+// import TestKit from "./pages/TestKit.tsx";
+// import TestModal from "./pages/TestModal.tsx";
+import TestCard from "./pages/TestCard.tsx";
+
+function App() {
+  return <TestCard />;
+}
+
+export default App;
