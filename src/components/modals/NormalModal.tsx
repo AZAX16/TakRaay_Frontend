49644fd9e79f9ba5ceea94e80cff63575daed833
MAHI <mvahedi@live.com>
2026-06-05 00:45:41 +0330

    feat: modal fixed

--- src/components/modals/NormalModal.tsx
+++ src/components/modals/NormalModal.tsx
@@ -5,7 +5,12 @@ type ModalProps = {
   children: React.ReactNode
 }
 
-export default function Modal({ isOpen, onClose, title, children }: ModalProps) {
+export default function Modal({
+  isOpen,
+  onClose,
+  title,
+  children,
+}: ModalProps) {
   if (!isOpen) return null
 
   return (
@@ -14,9 +19,14 @@ export default function Modal({ isOpen, onClose, title, children }: ModalProps) 
         onClick={onClose}
         className="fixed inset-0 bg-black/40 backdrop-blur-sm z-[900]"
       />
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#B8EAED] rounded-2xl shadow-xl z-[1000] w-[500px] max-w-[90%]">
-        <div className="flex justify-between items-center border-b-2 border-[#387FA3] p-4">
-          <h2 className="text-[#387FA3] font-bold text-lg">{title}</h2>
+
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#B8EAED] rounded-2xl shadow-xl z-[1000] w-[500px] max-w-[90%] max-h-[80vh] flex flex-col">
+        
+        <div className="flex justify-between items-center border-b-2 border-[#387FA3] p-4 shrink-0">
+          <h2 className="text-[#387FA3] font-bold text-lg">
+            {title}
+          </h2>
+
           <button
             onClick={onClose}
             className="text-[#387FA3] text-xl"
@@ -24,9 +34,15 @@ export default function Modal({ isOpen, onClose, title, children }: ModalProps) 
             ✕
           </button>
         </div>
-        <div className="p-5 text-[#387FA3] max-w-[600px] min-w-[175px]">
+
+        <div className="p-1"></div>
+
+        <div className="p-5 text-[#387FA3] overflow-y-auto faq-scroll">
           {children}
         </div>
+
+        <div className="p-2"></div>
+
       </div>
     </>
   )
